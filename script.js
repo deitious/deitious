a23c8748cb41673410b6335b09e32a77cf56a509
deitious <davismwanthi26@gmail.com>
2025-08-18 14:16:08 +0300

    Revert gallery back to normal

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const modal = document.getElementById('modal');
-const modalImg = document.getElementById('modal-img');
-const close = document.getElementById('close');
 
-document.querySelectorAll('.thumbnail').forEach(img => {
-    img.onclick = () => {
-        modal.style.display = 'block';
-        modalImg.src = img.src;
-    };
-});
-
-close.onclick = () => {
-    modal.style.display = 'none';
-};
